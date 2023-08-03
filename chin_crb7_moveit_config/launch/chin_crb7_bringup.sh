8b5aaffab3f0016361d28342bd0feef99a48d09f
Xinjue Zou <hit_zou@126.com>
2023-08-03 10:39:56 +0800

    add script file for chin crb7 shortcut

---
+++ chin_crb7_moveit_config/launch/chin_crb7_bringup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/whi/catkin_workspace/
+source /opt/ros/melodic/setup.bash
+source /home/whi/catkin_workspace/devel/setup.bash
+echo "launching application, please wait..."
+roslaunch chin_crb7_moveit_config chin_arm_moveit_bringup.launch 
